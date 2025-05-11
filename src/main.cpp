c198c57e7f31f228a0ee12438df23e92b539ddf5
Christie Joseph <christiejoseph12@gmail.com>
2025-05-11 17:42:56 +0100

    REPL

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,13 @@ int main()
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
+  while (true)
+  {
+    // Uncomment this block to pass the first stage
+    std::cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout << input << ": command not found" << std::endl;
+    std::string input;
+    std::getline(std::cin, input);
+    std::cout << input << ": command not found" << std::endl;
+  }
 }
