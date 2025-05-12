3e7bd2299fef145f3fbf95f556faeee6448dc359
Christie Joseph <christiejoseph12@gmail.com>
2025-05-12 10:38:28 +0100

    adding simple echo functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+void handleEcho(std::string &input);
+
 int main()
 {
   // Flush after every std::cout / std:cerr
@@ -18,6 +20,24 @@ int main()
     {
       return 0; // Exit the program from main
     }
-    std::cout << input << ": command not found" << std::endl;
+    else if (input.rfind("echo", 0) == 0) // if input starts with "echo "
+    {
+      // Output the part after "echo "
+      handleEcho(input);
+    }
+    else
+    {
+      std::cout << input << ": command not found" << std::endl;
+    }
   }
+}
+
+void handleEcho(std::string &input)
+{
+  if (input == "echo")
+  {
+    std::cout << std::endl;
+    return;
+  }
+  std::cout << input.substr(5) << std::endl;
 }
