ab66384ba7a61045b5353c7def6ddd32daa147a6
Christie Joseph <christiejoseph12@gmail.com>
2025-05-29 18:50:48 +0100

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <string>
-
-void handleEcho(std::string &input);
-void handleType(std::string arg);
+#include "command_utils.h"
 
 int main()
 {
@@ -12,72 +10,26 @@ int main()
 
   while (true)
   {
-    // Uncomment this block to pass the first stage
     std::cout << "$ ";
 
     std::string input;
     std::getline(std::cin, input);
+    
     if (input == "exit 0")
     {
       return 0; // Exit the program from main
     }
-    else if (input.rfind("echo", 0) == 0) // if input starts with "echo "
+    else if (isEchoCommand(input))
     {
-      // Output the part after "echo "
       handleEcho(input);
     }
-    else if ((input.rfind("type", 0) == 0) && input.length() > 4) // if input starts with "echo "
+    else if (isTypeCommand(input))
     {
-      // Output the part after "echo "
       handleType(input.substr(5));
     }
     else
     {
       std::cout << input << ": command not found" << std::endl;
     }
-  }
-}
-
-void handleEcho(std::string &input)
-{
-  if (input == "echo")
-  {
-    std::cout << std::endl;
-    return;
-  }
-  std::cout << input.substr(5) << std::endl;
-}
-
-void handleType(std::string arg)
-{
-  if (arg == "type")
-  {
-    std::cout << arg << " is a shell builtin" << std::endl;
-    return;
-  }
-  else if (arg == "echo")
-  {
-    std::cout << arg << " is a shell builtin" << std::endl;
-    return;
-  }
-  else if (arg == "exit")
-  {
-    std::cout << arg << " is a shell builtin" << std::endl;
-    return;
-  }
-  else if (arg == "cd")
-  {
-    std::cout << arg << " is a shell builtin" << std::endl;
-    return;
-  }
-  else if (arg == "pwd")
-  {
-    std::cout << arg << " is a shell builtin" << std::endl;
-    return;
-  }
-  else
-  {
-    std::cout << arg << ": not found" << std::endl;
-    return;
   }
 }
